365ae5f7da28397efcbe21aab9611ee977892e95
Hoel Bagard <hoel.bagard@gmail.com>
2022-11-01 23:05:21 +0900

    Fixed the mask typing of pycocotools.

--- pycocotools/mask.pyi
+++ pycocotools/mask.pyi
@@ -3,15 +3,14 @@ import numpy.typing as npt
 
 from .coco_types import EncodedRLE
 
-
 def encode(bimask: npt.NDArray[np.uint8]) -> EncodedRLE:
     ...
 
 def decode(rleObjs: EncodedRLE) -> npt.NDArray[np.uint8]:
     ...
 
-def area(rleObjs: EncodedRLE) -> npt.NDArray[np.uint32]:
+def area(rleObjs: EncodedRLE) -> np.uint32:
     ...
 
-def toBbox(rleObjs: EncodedRLE) -> npt.NDArray[np.uint32]:
+def toBbox(rleObjs: EncodedRLE) -> npt.NDArray[np.float64]:
     ...
